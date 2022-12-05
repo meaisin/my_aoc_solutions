ff06eac546eb808b4d533e7001a9f1a30f057655
ATP <atpdev24@gmail.com>
2022-12-05 06:43:59 +0000

    Add ugly but correct solution for day 5, 2022.

---
+++ 2022/day_05/Part1.scala
@@ -0,0 +1,82 @@
+class Crates(initialState: List[List[String]]):
+  val crateStacks = initialState.toArray
+
+  override def toString: String =
+    crateStacks.map(_.mkString(" ")).mkString("\n")
+
+  def moveCrates(quantity: Int, from: Int, to: Int): Unit =
+    if quantity == 0 then
+      ()
+    else
+      val crate = crateStacks(from - 1).head
+      crateStacks(from - 1) = crateStacks(from - 1).tail
+      crateStacks(to - 1) = crate :: crateStacks(to - 1)
+      moveCrates(quantity - 1, from, to)
+
+  def moveCratesTogether(quantity: Int, from: Int, to: Int): Unit =
+    val stack = crateStacks(from - 1).take(quantity)
+    crateStacks(from - 1) = crateStacks(from - 1).drop(quantity)
+    crateStacks(to - 1) = stack concat crateStacks(to - 1)
+
+  val topCrates: String =
+    crateStacks.toList.map(_.head).mkString
+
+def parseInstruction(instruction: String): (Int, Int, Int) =
+  import scala.util.matching.Regex
+  val instructionRegex = "move ([0-9]+) from ([0-9]+) to ([0-9]+)".r
+  instruction match
+    case instructionRegex(quantity, fromStack, toStack) =>
+      (Integer.parseInt(quantity), 
+        Integer.parseInt(fromStack), 
+        Integer.parseInt(toStack))
+    case _ =>
+      throw new RuntimeException("Contradiction.")
+
+def cleanValue(string: String): String =
+  import scala.util.matching.Regex
+  val crate = """\[([A-Z])\](\s*)""".r
+  string match
+    case crate(symbol, _) => symbol
+    case _ => "_"
+
+@main def main(arg: String): Unit =
+  import scala.io.Source
+  import scala.util.matching.Regex
+
+  val data = Source
+    .fromFile(arg)
+    .getLines
+    .toList
+    .mkString("\n")
+    .split("\n\n")
+
+  require(data.length == 2)
+
+  val crateText = data(0)
+  val instructionText = data(1).split("\n")
+
+  val instructions = instructionText.map(parseInstruction(_))
+
+  val stacks = crateText
+    .split("\n")
+    .map(_
+      .grouped(4)
+      .toList)
+    .init
+    .toList
+    .map(_.map(cleanValue(_)))
+    .transpose
+    .map(_.dropWhile(_ == "_"))
+
+  val part1Crates = Crates(stacks)
+  val part2Crates = Crates(stacks)
+
+  for
+    instruction <- instructions
+  do
+    val (qty, from, to) = instruction
+    part1Crates.moveCrates(qty, from, to)
+    part2Crates.moveCratesTogether(qty, from, to)
+
+  println(part1Crates.crateStacks.toList.map(_.head).mkString)
+  println(part2Crates.crateStacks.toList.map(_.head).mkString)

e9745a1f148351ebeed50ac8dfdb621c364d5e01
ATP <atpdev24@gmail.com>
2022-12-07 23:23:58 +0000

    Complete solution for day 7, 2022.

--- 2022/day_07/Solution.scala
+++ 2022/day_07/Solution.scala
@@ -1,4 +1,5 @@
 class File(val name: String, val size: Int):
+  override def toString: String = s"$name: $size bytes"
   val isDir: Boolean = false
 
 class Dir(name: String,
@@ -14,38 +15,59 @@ class Dir(name: String,
   def addChild(child: Dir): Unit =
     children = child :: children
 
-  override val size: Int =
-    files.map{x =>
-      x match
-        case d: Dir => d.files.map(_.size).sum
-        case f: File => f.size
-    }.sum
+  def fileSize: Int =
+    files.map(_.size).sum + children.map(_.fileSize).sum
 
-@main def main(arg: String): Dir =
+  override def toString: String =
+    s"$name: ${fileSize}"
+
+object Helpers:
+  def parseDir(lines: List[String]): Dir =
+    val rootDir = Dir("/", None, List(), List())
+    var currentDir = rootDir
+    for
+      line <- lines.tail
+    do
+      line.split(" ").toList: List[String] match
+        case List("$", "ls") => ()
+        case List("$", "cd", "..") => currentDir = currentDir.parentDir match
+          case Some(p) => p
+          case None => currentDir
+        case List("$", "cd", child) => currentDir = currentDir.children.find(_.name == child) match
+          case Some(childDir) => childDir
+          case None => throw new RuntimeException(s"Child not found: $child")
+        case List("dir", name) => currentDir.addChild(Dir(name, Some(currentDir), List(), List()))
+        case List(size, name) => currentDir.addFile(File(name, Integer.parseInt(size)))
+        case x => throw new RuntimeException(s"You fucked up the pattern matching. Here: $x")
+    rootDir
+
+  def getDirSizes(dir: Dir): List[Int] =
+    def inner(list: List[Dir], output: List[Int]): List[Int] =
+      if list.isEmpty then
+        output
+      else
+        inner(list.map(_.children).flatten, list.map(_.fileSize) concat output)
+    inner(List(dir), List())
+
+@main def main(arg: String): Unit =
   import scala.io.Source
-  import scala.util.matching.Regex
 
   val data = Source
     .fromFile(arg)
     .getLines
     .toList
 
-  val rootDir = Dir("/", None, List(), List())
-  var currentDir = rootDir
+  val rootDir = Helpers.parseDir(data)
+  val sizes = Helpers.getDirSizes(rootDir)
+  val part1Solution = sizes.filter(_ < 100000).sum
+  println(part1Solution)
 
-  for
-    line <- data.tail
-  do
-    line.split(" ").toList: List[String] match
-      case List("$", "ls") => ()
-      case List("$", "cd", "..") => currentDir = currentDir.parentDir match
-        case Some(p) => p
-        case None => currentDir
-      case List("$", "cd", child) => currentDir = currentDir.children.find(_.name == child) match
-        case Some(childDir) => childDir
-        case None => throw new RuntimeException(s"Child not found: $child")
-      case List("dir", name) => currentDir.addChild(Dir(name, Some(currentDir), List(), List()))
-      case List(size, name) => currentDir.addFile(File(name, Integer.parseInt(size)))
-      case x => throw new RuntimeException(s"You fucked up the pattern match. Here: $x")
+  val totalSpace = 70000000
+  val updateSpace = 30000000
 
-  rootDir
+  val usedSpace = rootDir.fileSize
+  val unusedSpace = totalSpace - usedSpace
+  val neededSpace = updateSpace - unusedSpace
+
+  val part2Solution = sizes.filter(_ >= neededSpace).min
+  println(part2Solution)

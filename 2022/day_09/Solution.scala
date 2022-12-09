82726fb1de222da78f41e2d4f2aee40e54467666
ATP <atpdev24@gmail.com>
2022-12-09 12:20:05 +0000

    Part 1, day 9, 2022.

--- 2022/day_09/Solution.scala
+++ 2022/day_09/Solution.scala
@@ -1,37 +1,53 @@
-enum Direction:
-  case Up
-  case Down
-  case Left
-  case Right
-
 class Rope:
   var headPos = (0, 0)
   var tailPos = (0, 0)
 
   private var lastHeadPos: Option[(Int, Int)] = None
 
-  var headVisited: Map[(Int, Int), Int] = Map.empty[(Int, Int), Int]
-  var tailVisited: Map[(Int, Int), Int] = Map.empty[(Int, Int), Int]
+  private var tailVisited: Map[(Int, Int), Int] = Map((0, 0) -> 1)
 
-  private def posDelta(headPos: (Int, Int), tailPos: (Int, Int)): (Int, Int) =
+  def moveHeadBy(dir: Char, by: Int): Unit =
+    if by == 0 then
+      ()
+    else
+      moveHead(dir)
+      moveHeadBy(dir, by - 1)
+
+  def uniqueTailPositions: Int =
+    tailVisited.size
+
+  private def moveHead(dir: Char): Unit =
+    val (hX, hY) = headPos
+    lastHeadPos = Some(headPos)
+    headPos = dir match
+      case 'U' => (hX, hY + 1)
+      case 'D' => (hX, hY - 1)
+      case 'L' => (hX - 1, hY)
+      case 'R' => (hX + 1, hY)
+    moveTail
+
+
+  private def moveTail: Unit =
+    if isTouching then
+      ()
+    else
+      tailPos = lastHeadPos match
+        case Some(pos) => pos
+        case None => throw new RuntimeException("No previous head position for tail to move to.")
+      tailVisited = tailVisited + (tailPos -> tailVisited.getOrElse(tailPos, 0).+(1))
+
+
+  private def posDelta: (Int, Int) =
     val (hX, hY) = headPos
     val (tX, tY) = tailPos
     (hX - tX, hY - tY)
 
-  private def isTouching(headPos: (Int, Int), tailPos: (Int, Int)): Boolean =
-    posDelta(headPos, tailPos) match
+  private def isTouching: Boolean =
+    posDelta match
       case (0, 0) | (0, 1) | (1, 0) | (1, 1) => true
       case (-1, 0) | (0, -1) | (-1, -1) => true
       case (1, -1) | (-1, 1) => true
       case _ => false
-
-    private def adjustTailPos(headPos: (Int, Int), tailPos: (Int, Int), lastHeadPos: Option[(Int, Int)]) =
-      if isTouching(headPos, tailPos) then
-        tailPos
-      else 
-        lastHeadPos match
-          Some(pos) => pos
-          None => throw new RuntimeException()
 
 end Rope
 
@@ -43,4 +59,13 @@ end Rope
     .getLines
     .toList
 
-  println(data.length)
+  val steps = data
+    .map(x => (x.head, Integer.parseInt(x.drop(2))))
+
+  val rope: Rope = Rope()
+
+  steps.foreach{x =>
+    rope.moveHeadBy(x._1, x._2)
+  }
+
+  println(rope.uniqueTailPositions)
